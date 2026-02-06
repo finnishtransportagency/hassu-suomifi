279f6613d8fc4b1e91668271b5bac4d29800c4cd
Jani Aaltonen <122603649+aaltonja@users.noreply.github.com>
2026-02-06 08:45:38 +0200

    HASSUYP-653 - Vaihdetaan Dev RDS Serverless V1 -> V2 (#13)
    * päivitä aws-cdk viimeisimpään versioon, korjaa schema versio virheen. Virhe tulee kun aws-cdk-lib on liian uusi jota aws-cdk:n ko. versio ei ymmärrä
    
    * vaihdetaan serverlessV2, ei toimi aiemman V1 kanssa. Säädetty samalla min ja max kapasiteetit pienemmiksi
    
    * korjaa v2 kapasiteetin tyyppi numeeriseksi. Min kapasiteetti arvon pitää olla 0, jotta auto pause toimii.
    
    * korjaa kommentti

--- lib/cdkpipelines-suomifi-stack.ts
+++ lib/cdkpipelines-suomifi-stack.ts
@@ -44,9 +44,9 @@ export class CdkpipelinesSuomifiStack extends Stack {
 
     // 3. Aurora postgresql -> Aurora Serverless
     let rdsinstance;
-    // not ready to change dev instances yet
+    // keep dev instance smaller and use auto pause feature
     if (environment === "dev") {
-      rdsinstance = new rds.ServerlessClusterFromSnapshot(this, "Cluster", {
+      rdsinstance = new rds.DatabaseClusterFromSnapshot(this, "Cluster", {
         engine: rds.DatabaseClusterEngine.auroraPostgres({
           version: rds.AuroraPostgresEngineVersion.VER_14_20,
         }),
@@ -54,12 +54,12 @@ export class CdkpipelinesSuomifiStack extends Stack {
         vpcSubnets: {
           subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
         },
+        writer: rds.ClusterInstance.serverlessV2("writer"),
         snapshotIdentifier: `arn:aws:rds:eu-west-1:283563576583:cluster-snapshot:keycloak-db-backup-pq-ver-14`,
-        scaling: {
-          autoPause: Duration.minutes(30),
-          minCapacity: rds.AuroraCapacityUnit.ACU_2,
-          maxCapacity: rds.AuroraCapacityUnit.ACU_8,
-        },
+        snapshotCredentials: rds.SnapshotCredentials.fromGeneratedSecret("postgres"),
+        serverlessV2AutoPauseDuration: Duration.minutes(30),
+        serverlessV2MinCapacity: 0,
+        serverlessV2MaxCapacity: 4
       });
     } else {
       rdsinstance = new rds.DatabaseClusterFromSnapshot(this, "Cluster", {
